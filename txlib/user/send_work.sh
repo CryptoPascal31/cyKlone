cf8ac63fef49df123d8ecacce51ccaef013f2e75
CryptoPascal31 <CryptoPascal31@users.noreply.github.com>
2023-03-23 16:02:13 +0100

    Add user txlib

---
+++ txlib/user/send_work.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+rm -f work*.yaml work*.json
+
+for i in $(seq 6); do
+    kda gen -t work.tkpl -d testnet.yaml -o work-$i.yaml
+done
+
+kda sign work*.yaml -k tmp.key.yaml
+
+for i in $(seq 6); do
+  kda send work-$i.json | jq
+done
