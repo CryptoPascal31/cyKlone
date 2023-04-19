a0614b35670c789f29dfc859461ced6f2346ebec
CryptoPascal31 <CryptoPascal31@users.noreply.github.com>
2023-04-19 15:27:06 +0200

    Improve Merkle tree download:    - More simple    - Avoid missing the last leaf in some cases

--- cyklone_js/cyklone_tree.js
+++ cyklone_js/cyklone_tree.js
@@ -6,7 +6,7 @@ import {MerkleTree} from "fixed-merkle-tree"
 import {b64_to_dec} from "./codecs.js"
 
 const ZERO = "8355858611563045677440680357889341193906656246723581940305640971585549179022";
-const MAX_DOWNLOAD_LEAF = 100
+const MAX_DOWNLOAD_LEAF = 128;
 
 /* Encoding and Decoding functions */
 const td = new TextDecoder('ascii');
@@ -51,11 +51,6 @@ class CyKloneTree
     return this.kadena_local(`(${MODULE}.get-deposits-range "${this.pool}" ${start} ${end})`);
   }
 
-  current_rank()
-  {
-    return this.kadena_local(`(at 'current-rank (${MODULE}.get-state "${this.pool}"))`);
-  }
-
   insert_commitments(chunk)
   {
     this.tree.bulkInsert(chunk.map(b64_to_dec));
@@ -63,24 +58,21 @@ class CyKloneTree
 
   async update()
   {
-    const rank = await this.current_rank()
-    let tree_size = this.tree.elements.length
-    if(tree_size === rank)
-    {
-      console.log("Merkle_tree up to date")
-      return
-    }
 
-    console.log(`Updating merkle tree ${tree_size} => ${rank}`)
-
-    while(tree_size < rank)
+    let dl_chunk_size = MAX_DOWNLOAD_LEAF;
+    while(dl_chunk_size === MAX_DOWNLOAD_LEAF)
     {
-      let chunk = await this.get_deposit_chunk(tree_size, Math.min(tree_size + MAX_DOWNLOAD_LEAF, rank-1));
-      console.log(`Updating Progress ${tree_size} => ${tree_size + chunk.length}`)
-      this.insert_commitments(chunk)
-      tree_size = this.tree.elements.length
+      const start_index = this.tree.elements.length;
+      const end_index = start_index + MAX_DOWNLOAD_LEAF - 1;
+      const chunk = await this.get_deposit_chunk(start_index, end_index);
+      dl_chunk_size = chunk.length;
+      if(dl_chunk_size > 0)
+      {
+        console.log(`Merkle Tree update Progress ${start_index} => ${start_index + dl_chunk_size}`)
+        this.insert_commitments(chunk)
+      }
     }
-    console.log("Update complete")
+    console.log("Merkle Tree update complete")
   }
 
   dump()
